87b915c83403ecc1fdc36516d0de18cbe56fbcb4
Jonas Schropp <jonas.schropp@protonmail.com>
2026-01-27 12:12:55 +0100

    added package installation script

---
+++ code/installations.R
@@ -0,0 +1,18 @@
+install.packages('renv')
+
+packages <- c("elizagrames/litsearchr", "easyPubMed", "revtools",
+              "dplyr", "tidyr", "ggplot2", "ggrepel",
+              "rvest", "here", "hrbrmstr/cfhttr",
+              "nealhaddaway/GSscraper", "readr",
+              "pdftools", "textclean", "tm", "igraph",
+              "tidygraph", "ggraph", "quanteda",
+              "rscopus", "stringr", "rentrez", "xml2",
+              "RefManageR", "stringdist", "fuzzyjoin",
+              "reticulate", "tidytext", "tm", "text2vec",
+              "topicmodels", "reshape2", "writexl", "readxl",
+              "roadoi", "brms", "devtools",
+              "mice")
+
+renv::install(packages)
+y
+ 
